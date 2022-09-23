f9dd8a05a188f0b61577149b95497ad02d7d54b4
Admin <Admin@DESKTOP-EQ55Q8H>
2022-09-23 11:19:40 +0530

    first commit

---
+++ B_25__javaproject/src/com/tns/assignment2/Student.java
@@ -0,0 +1,12 @@
+package com.tns.assignment2;
+
+public class Student {
+	Student(){
+		 System.out.println("The object of student is created");
+	 }
+		public static void main(String[] args)
+		{
+			
+			Student xc=new Student();
+	}
+}
---
+++ B_25__javaproject/src/com/tns/assignment2/UseThisKeyword.java
@@ -0,0 +1,12 @@
+package com.tns.assignment2;
+public class UseThisKeyword {
+int c;
+public  UseThisKeyword(int c){
+	this.c=c;
+}
+public static void main(String[] args) {
+	UseThisKeyword vc=new UseThisKeyword(5);
+	System.out.println("the value of this c is ="+vc.c);
+}
+
+}
